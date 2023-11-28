0815385faea923ece0cd9eade5cd410b4b4fdd53
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 13:35:29 +0000

    change in location

---
+++ 01_basics/dataypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+//7   types: String, Number, Boolean , null, undefined, Sumbol, BigInt
+
+const score = 100 
+const scoreValue= 100.3
+
+const isLoggedIn = false 
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('321')
+
+const bigNumber = 23423423423432423n
+
+
+
+
+
+// Reference (Non primitive)
+
+// Array , Objects, Functions 
+
+
+
+const heros=["Rohit", "Nishant", "doga", "Dipak"];
+
+let myObj = {
+    name: "Rohit", 
+    age: 20,
+}
+
+const myfunction = function (){
+    console.log("Hello world")
+}
+
+console.log(typeof(myfunction))
