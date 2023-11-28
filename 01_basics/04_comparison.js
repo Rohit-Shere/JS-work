1706d838444fe7a96cd4f721943497b7911dcbbe
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 13:10:57 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+//console.log(2>1);
+//console.log(3<=1)
+// console.log(2==1)
+//console.log(2!=1)
+
+console.log("2">1)
+console.log("02"<0)
+
+
+console.log(null<0);
+console.log(null==0)
+console.log(null>=0)
+
+//=== -->striclty comparison
+console.log("2"===2)
