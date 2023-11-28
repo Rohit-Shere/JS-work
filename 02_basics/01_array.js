b90b35ee6620e0137094bfbb270139967a946e38
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 19:53:15 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// array 
+
+const myArr = [1,2,3,4,5]
+
+// console.log(myArr)
+
+const myHerros= ['Shaktiman', 'Ram']
+
+const myArr2= new Array(1,2,3,4)
+// console.log(myArr2)
+
+// Array methods
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(2));
+
+const newArr= myArr.join()
+
+// console.log(newArr);
+
+
+console.log("A",myArr);
+
+const myn1= myArr.slice(1,3)
+console.log("B", myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myn2);
+console.log(myArr)
