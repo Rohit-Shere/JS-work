8f632452a5a12b8f60f6241e1bddc76ca14431ac
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 17:26:48 +0000

    date

---
+++ 01_basics/07_dates_time.js
@@ -0,0 +1,31 @@
+
+// Dates
+
+// let myDate= new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate)
+
+
+let myDate = new Date()
+
+console.log(myDate.toLocaleDateString())
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.getMonth()+1)
+console.log(myDate.getDay());
+
+
+newDate.toLocaleDateString()('default',{
+    weekday:'long'
+    
+})
