0940e1e720b3072b22fb87cc5279120afe24594c
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 12:29:52 +0000

    data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13443
+let accountEmail= "abc@gmail.com"
+var accountPassword = "2343"
+accountCity= "Pune"
+
+
+// accountId= 2
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not use to var
+only use let and const 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3)//we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Rohit")
+
+ //number=> 2 to power 52
+ // bigint
+ //string=> ""
+ //boolean => true / false
+ // null=> standalone value
+ //undefined=>
+ //symbol => unique
+
+
+ // object 
+ console.log(typeof null)
+
+ 
