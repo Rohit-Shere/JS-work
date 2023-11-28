6fda5405ef2f7fd5c64c9f937ca33ec089ae811f
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 13:03:02 +0000

    rest ops

--- 01_basics/03_conversioOperations.js
+++ 01_basics/03_conversioOperations.js
@@ -19,4 +19,12 @@ console.log(booleanisLoggedIN)
 //""=> false
 //"Rohit"=> true
 
+let value=3
+let negValue= -value
+//console.log(negValue)
 
+let str1="hello"
+let str2= " Nishu"
+
+let str3= str1+str2 
+console.log(str3)
