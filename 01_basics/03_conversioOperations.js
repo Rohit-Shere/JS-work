da10aac59ae81b4b214df633d4085ebdaad59fec
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-11-28 12:47:54 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversioOperations.js
@@ -0,0 +1,22 @@
+let score =true
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber= Number(score )
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33"=> 33
+//"33asd"=>NaN
+//true=>1; false=> 0
+
+let isLoggedIn=1
+
+let booleanisLoggedIN= Boolean(isLoggedIn)
+console.log(booleanisLoggedIN)
+
+//1=> true, 0=>false
+//""=> false
+//"Rohit"=> true
+
+
