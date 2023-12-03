c51883b6b1218a0daaab4781448304a90b28128b
crazyrohit <106076471+crazyrohit@users.noreply.github.com>
2023-12-03 10:45:25 +0000

    fuctions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,6 +29,31 @@ function loginUserMessage(username="user"){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
 
+function calculateCartPrice (val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "Rohit",
+    Price :199
+}
+
+function handleObject(anyObject ){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.Price} `);
+}
+
+handleObject(user)
+
+const myNewArray = [200,400,100,500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,500]));
 
